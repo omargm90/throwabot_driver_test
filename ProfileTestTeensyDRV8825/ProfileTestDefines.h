b0b7f4743cfe488545e5d75ecba41841ad95c573
omar guijarro <o.g.martinez@tmc.nl>
2021-02-24 21:03:04 +0100

    +modified pin defines for DRV8825

--- ProfileTestTeensyDRV8825/ProfileTestDefines.h
+++ ProfileTestTeensyDRV8825/ProfileTestDefines.h
@@ -1,9 +1,9 @@
 #define DIR_PIN         3 // Direction
 #define STEP_PIN        4 // Step
-#define SLEEP_PIN       5 // Step
-#define RST_PIN         6 // Reset 
-#define M2_PIN          7 // M2 pin
-#define M1_PIN          8 // M1 pin
+#define SLEEP_PIN       7 // Step
+#define RST_PIN         8 // Reset 
+#define M2_PIN          5 // M2 pin
+#define M1_PIN          6 // M1 pin
 #define M0_PIN          9 // M0 pin
 #define EN_PIN          10 // Enable
 
